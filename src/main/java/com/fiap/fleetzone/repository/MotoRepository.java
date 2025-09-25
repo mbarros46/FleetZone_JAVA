7bb56842a22bfee17133bcf8cf6eca53432f09da
mbarros46 <miguelbarrosramos46@gmail.com>
2025-09-25 18:35:11 -0300

    feat: Adiciona funcionalidades de transferência de motos e relatórios por pátio

--- src/main/java/com/fiap/fleetzone/repository/MotoRepository.java
+++ src/main/java/com/fiap/fleetzone/repository/MotoRepository.java
@@ -2,10 +2,13 @@
 package com.fiap.fleetzone.repository;
 
 import com.fiap.fleetzone.model.Moto;
+import com.fiap.fleetzone.model.Patio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface MotoRepository extends JpaRepository<Moto, Long> {
     List<Moto> findByStatus(String status);
+    List<Moto> findByPatio(Patio patio);
+    List<Moto> findByPatioId(Long patioId);
 }
